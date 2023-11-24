363d0259011d081542e60e12a10aa53d65b3c569
MrSpudniks <MrSpudniks@gmail.com>
2023-11-24 18:38:07 +0100

    added temperature control and cell generation wip

--- heatTransfer/ht.js
+++ heatTransfer/ht.js
@@ -1,22 +1,47 @@
+let x = 1;
+let y = 1;
+let maxX = 10;
+let cells = 100;
+let timeSinceLastScroll = 1;
+let timeAtLastScroll = 1;
+let currentTime = 0;
+let scrollLength = 1;
+let clones = [];
 
-let cloneId = 1;
+document.addEventListener("onwheel" in document ? "wheel" : "mousewheel", function(e) {
+    e.wheel = (e.deltaY ? -e.deltaY : e.wheelDelta/40) / 100;
+
+    time = new Date();
+    currentTime = time.getTime();
+    timeSinceLastScroll = currentTime - timeAtLastScroll;
+    timeAtLastScroll = currentTime;
+    scrollLength = Math.round(e.wheel * Math.max(1, 100 / timeSinceLastScroll));
+});
 
 class cell {
-    constructor() {
-        
-    }
-}
+    constructor(X, Y) {
+        this.X = X;
+        this.Y = Y;
+        this.temp = 50;
+    };
 
-function add() {
+    transfer() {
+
+    };
+};
+
+for (let i = 1; i < (cells + 1); i++) {
     newthing = document.createElement("div");
-    newthing.id = cloneId;
+    x = i % 10;
+    y = Math.floor(i / 10);
+    clones.push(newthing);
     newthing.classList.add('grid');
-    cloneId++;
-    document.getElementById("grid").appendChild(newthing);
-}
+    // document.getElementById("grid").appendChild(newthing);
+    newthing = new cell(x, y);
+};
 
-setInterval (update, 50);
+setInterval(update, 50);
 
 function update() {
-    // rtthrthrrw
-}
+    // clones.forEach( (item)=> item.transfer() );
+};
