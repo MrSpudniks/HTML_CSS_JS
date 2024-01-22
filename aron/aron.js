a12dcd656fa9d4cac73845fb54e534ec0d4ec43c
MrSpudniks <MrSpudniks@gmail.com>
2024-01-22 12:24:08 +0100

    tehetaha + new project for mr aron mann

--- aron/aron.js
+++ aron/aron.js
@@ -1,23 +1,21 @@
 
 const CELLS = 10000;
-
-
+const GRID = document.getElementById("gridContainer");
 
 
 
 window.addEventListener("resize", () => {
     viewW = window.innerWidth;
     viewH = window.innerHeight;
-    grid.style.height = `${Math.min(viewH, viewW - 210)}px`;
-    grid.style.width = `${Math.min(viewH, viewW - 210)}px`;
-    grid.style.fontSize = `${Math.min(viewH, viewW - 210)/50}px`;
+    GRID.style.height = `${Math.min(viewH, viewW - 210)}px`;
+    GRID.style.width = `${Math.min(viewH, viewW - 210)}px`;
+    GRID.style.fontSize = `${Math.min(viewH, viewW - 210)/50}px`;
 });
 
 
-for (let i = 1; i < (cells + 1); i++) {
+for (let i = 1; i < (CELLS + 1); i++) {
     newCell = document.createElement("div");
-    newCell.id = cloneIt;
-    cloneIt++;
+    newCell.id = i;
     newCell.classList.add('gridContainer');
-    document.getElementById("gridContainer").appendChild(newCell);
+    GRID.appendChild(newCell);
 };
